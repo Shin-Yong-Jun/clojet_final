02e36a954ac8172a188b8d050bb3c97ab214ca50
HEESU <shs6429@gmail.com>
2023-06-09 17:07:39 +0900

    카테고리 사이드 대충 정리 완

--- src/main/frontapp/src/components/category/categoryN.js
+++ src/main/frontapp/src/components/category/categoryN.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import cNdata from './cNdata';
 import './categoryN.scss';
 import { Link } from 'react-router-dom';
-import { Check, CheckBox } from '@mui/icons-material';
 import ArrowDropDownRoundedIcon from '@mui/icons-material/ArrowDropDownRounded';
 
 function CategoryN() {
     const size = ['XS', 'S', 'M', 'L', 'XL', '2XL'];
+    const color = ['red', 'orange', 'yellow', 'green', 'blue'];
+    const item = ['outer', 'top', 'pants'];
+
     const [first, setfirst] = useState(Array(size.length).fill(false));
 
     function clickE(index) {
@@ -23,13 +25,13 @@ function CategoryN() {
                 <div className='category_side'>
                     <h1>NEW</h1>
                     <div className='side_item'>
-                        <Link to={'/'}>아우터</Link>
-                        <Link to={'/'}>top</Link>
-                        <Link to={'/'}>아우터</Link>
-                        <Link to={'/'}>아우터</Link>
-                        <Link to={'/'}>아우터</Link>
-                        <Link to={'/'}>아우터</Link>
-                        <Link to={'/'}>아우터</Link>
+                        {item.map((itemV, index) => {
+                            return (
+                                <div className='item_type'>
+                                    <Link to={'/'}>{itemV}</Link>
+                                </div>
+                            );
+                        })}
                     </div>
                     <div className='side_item'>
                         <div className='side_title'>
@@ -44,31 +46,38 @@ function CategoryN() {
                             <input type='checkbox' />
                             <span> woman</span>
                         </div>
+                        <div>
+                            <input type='checkbox' />
+                            <span> unisex</span>
+                        </div>
                     </div>
                     <div className='side_item'>
                         <div className='side_title'>
                             <h3>가격대</h3>
                             <ArrowDropDownRoundedIcon className='icon' />
                         </div>
-                        <div>
-                            <input type='checkbox' />
-                            <span> 0 - 100,000 원</span>
+
+                        <div className='price_box'>
+                            <label for='price'>
+                                <input type='checkbox' id='price' />
+                                <span className='price'> 0 - 100,000원</span>
+                            </label>
                         </div>
                         <div>
                             <input type='checkbox' />
-                            <span> 100,000 - 200,000 원</span>
+                            <span className='price'> 100,000 - 200,000 원</span>
                         </div>
                         <div>
                             <input type='checkbox' />
-                            <span> 200,000 - 300,000 원</span>
+                            <span className='price'> 200,000 - 300,000 원</span>
                         </div>
                         <div>
                             <input type='checkbox' />
-                            <span> 300,000 - 400,000 원</span>
+                            <span className='price'> 300,000 - 400,000 원</span>
                         </div>
                         <div>
                             <input type='checkbox' />
-                            <span> 400,000 - 500,000 원</span>
+                            <span className='price'> 400,000 - 500,000 원</span>
                         </div>
                     </div>
                     <div className='side_item'>
@@ -77,10 +86,18 @@ function CategoryN() {
                             <ArrowDropDownRoundedIcon className='icon' />
                         </div>
                         <div className='pallet'>
-                            <div className='color_box'>
-                                <div className='color'>{'동그라미'}</div>
-                                <div className='color_name'>{'색상'}</div>
-                            </div>
+                            {color.map((colorV, index) => {
+                                return (
+                                    <div className='color_box'>
+                                        <div
+                                            className={`color ${colorV}`}
+                                        ></div>
+                                        <div className='color_name'>
+                                            {colorV}
+                                        </div>
+                                    </div>
+                                );
+                            })}
                         </div>
                     </div>
                     <div className='side_item'>
@@ -116,31 +133,31 @@ function CategoryN() {
                 </div>
                 <div className='category_item'>
                     <h1>아이템</h1>
+                    <div className='container'>
+                        {cNdata.map((product, index) => (
+                            <div key={product.id} className='pd_box'>
+                                <div className='pd_img'>
+                                    <a href='/'>
+                                        <img
+                                            src={require(`../../image/product${
+                                                index + 1
+                                            }.jpg`)}
+                                            alt={`product${index + 1}`}
+                                        />
+                                    </a>
+                                </div>
+                                <div className='pd_info'>
+                                    <p>{product.title}</p>
+                                    <p>{product.PercentS}</p>
+                                    <p>
+                                        <del>{product.basicP}</del>{' '}
+                                        <strong>{product.saleP}</strong>
+                                    </p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-                {/* <div className='container'>
-                    {cNdata.map((product, index) => (
-                        <div key={product.id} className='pd_box'>
-                            <div className='pd_img'>
-                                <a href='/'>
-                                    <img
-                                        src={require(`../../image/product${
-                                            index + 1
-                                        }.jpg`)}
-                                        alt={`product${index + 1}`}
-                                    />
-                                </a>
-                            </div>
-                            <div className='pd_info'>
-                                <p>{product.title}</p>
-                                <p>{product.PercentS}</p>
-                                <p>
-                                    <del>{product.basicP}</del>{' '}
-                                    <strong>{product.saleP}</strong>
-                                </p>
-                            </div>
-                        </div>
-                    ))}
-                </div> */}
             </div>
         </div>
     );
