3e03a60c1f179c0bf807927c5b97ad85a32cdca4
Your Name <necron7577@daum.net>
2023-06-14 14:55:03 +0900

    디테일

--- src/main/frontapp/src/pages/detail.js
+++ src/main/frontapp/src/pages/detail.js
@@ -1,13 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/detail.scss';
-
-import GradeRoundedIcon from '@mui/icons-material/GradeRounded';
+import OpenInNewRoundedIcon from '@mui/icons-material/OpenInNewRounded';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
+import ReceiptLongRoundedIcon from '@mui/icons-material/ReceiptLongRounded';
+import CreditCardOffOutlinedIcon from '@mui/icons-material/CreditCardOffOutlined';
+import PermPhoneMsgOutlinedIcon from '@mui/icons-material/PermPhoneMsgOutlined';
+import LooksOneRoundedIcon from '@mui/icons-material/LooksOneRounded';
+import LooksTwoRoundedIcon from '@mui/icons-material/LooksTwoRounded';
 
 function Detail() {
     // DB
-    const original_price = 600000;
-    const discount = 50;
-    const rating = 4.2;
+    const size = [80, 110];
+    const color = ['red', 'blue'];
+
+    const [sizeState, setSizeState] = useState(Array(size.length).fill(false));
+    const [colorState, setColorState] = useState(Array(size.length).fill(false));
+    const [divCount, setDivCount] = useState(0);
+
+    function hasTrueValue() {
+        if (!sizeState.some((value) => value === true) || !colorState.some((value) => value === true)) {
+            alert('옵션을 선택해주세요');
+        } else {
+            alert('구매페이지로 이동하기');
+        }
+    }
+
+    function renderDivs() {
+        const divs = [];
+        for (let i = 0; i < divCount; i++) {
+            divs.push(<div key={i}>Div {i + 1}</div>);
+        }
+        return divs;
+    }
+
+    function clickE(setState, index) {
+        setState((prev) => {
+            const newState = [...prev];
+
+            if (!newState[index]) {
+                newState.fill(false);
+                newState[index] = true;
+            } else {
+                newState[index] = false;
+            }
+
+            return newState;
+        });
+    }
+
+    function OtherInformation(icon, title, subtitle) {
+        return (
+            <div>
+                <div className='icon'>{icon}</div>
+                <div className='title'>
+                    <p>{title}</p>
+                    <span>{subtitle}</span>
+                </div>
+                <div className='icon'>
+                    <OpenInNewRoundedIcon />
+                </div>
+            </div>
+        );
+    }
 
     return (
         <div className='detail'>
@@ -28,72 +82,79 @@ function Detail() {
                 </div>
 
                 <div className='detail_top_information'>
-                    <div className='event_logo text-red'>
-                        <span>event_logo</span>
-                        <span>event_logo</span>
+                    <div className='title_box'>
+                        <h1 className='title'>titletitletitletitletitl etitletitletitletitle</h1>
+                        <h2 className='subtitle'>titletitletitletitletitl etitletitletitletitle</h2>
                     </div>
 
-                    <div className='hashTag text-red'>
-                        <span>#hashTag</span>
-                        <span>#hashTag</span>
-                        <span>#hashTag</span>
+                    <div className='pirce_box'>
+                        <span className='discount text-red'>50%</span>
+                        <del>3000,000원</del>
+                        <p>2,000,000원</p>
                     </div>
 
-                    <h1 className='title'>titletitletitletitletititle titletitletitletletitletitle</h1>
-
-                    <div className='price_area border-top'>
-                        <strong className='discount text-red'>{'50%'}</strong>
-                        <strong className='price'>
-                            {(original_price * discount) / 100}
-                            <span className='won'>원</span>
-                        </strong>
-                        <small className='text-gray'>
-                            <del className='original_price'>{original_price}</del>
-                            <span className='won'>원</span>
-                        </small>
-                    </div>
-
-                    <div className='about_rating border-top'>
-                        <div className='rating_star'>
-                            <GradeRoundedIcon />
-                            <GradeRoundedIcon />
-                            <GradeRoundedIcon />
-                            <GradeRoundedIcon />
-                            <GradeRoundedIcon />
+                    <div className='size'>
+                        <span>
+                            <LooksOneRoundedIcon />
+                            SIZE
+                        </span>
+                        <div className='select'>
+                            {size.map((value, index) => {
+                                return (
+                                    <div
+                                        className={sizeState[index] && 'check'}
+                                        key={index}
+                                        onClick={() => clickE(setSizeState, index)}
+                                    >
+                                        {value}
+                                    </div>
+                                );
+                            })}
                         </div>
-                        <span className='rating'>{rating}</span>
                     </div>
 
-                    <div className='delivery_info border-top'>
-                        <h4>배송정보</h4>
-                        <dl>
-                            <dt>배송유형</dt>
-                            <dd>{'일반배송'}</dd>
-                        </dl>
-                        <dl>
-                            <dt>배송비</dt>
-                            <dd>{'3000'}</dd>
-                            <span className='won'>원</span>
-                        </dl>
+                    <div className={`color ${sizeState.some((value) => value === true) ? null : 'hidden'}`}>
+                        <span>
+                            <LooksTwoRoundedIcon />
+                            color
+                        </span>
+                        <div className='select'>
+                            {color.map((value, index) => {
+                                return (
+                                    <div
+                                        className={colorState[index] && 'check'}
+                                        key={index}
+                                        onClick={() => {
+                                            clickE(setColorState, index);
+                                        }}
+                                    >
+                                        {value}
+                                    </div>
+                                );
+                            })}
+                        </div>
                     </div>
 
-                    <div className='payment_info border-top'>
-                        <h4>가격정보</h4>
-                        <dl>
-                            <dt>판매가</dt>
-                            <dd>{'600,000'}</dd>
-                            <span className='won'>원</span>
-                        </dl>
-                        <dl>
-                            <dt>포인트적립</dt>
-                            <dd>{'600,000'}</dd>
-                            <span className='won'>원</span>
-                        </dl>
+                    <div>
+                        {renderDivs()}
+                        <h1>tititititititititit</h1>
+                        <h1>300000000000원</h1>
                     </div>
 
-                    <div className='buy_info border-top'>buy_info</div>
+                    <div className='buy_box'>
+                        <div>장바구니</div>
+                        <div onClick={() => hasTrueValue()}>구매하기</div>
+                    </div>
+
+                    <div className='otherInformation'>
+                        {OtherInformation(<LocalShippingOutlinedIcon />, '설명문', '설명문')}
+                        {OtherInformation(<ReceiptLongRoundedIcon />, '설명문')}
+                        {OtherInformation(<CreditCardOffOutlinedIcon />, '설명문')}
+                        {OtherInformation(<PermPhoneMsgOutlinedIcon />, '설명문')}
+                    </div>
                 </div>
             </div>
+
             <div className='detail_bottom'>
                 <div className='detail_bottom_header'>detail_bottom_header</div>
                 <div className='detail_bottom_information'>detail_bottom_information</div>
