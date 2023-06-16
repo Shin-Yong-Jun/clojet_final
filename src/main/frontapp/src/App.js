46b3e6c2ca95a3a821efe7724b29350325b1a891
hakro <leehr125@naver.com>
2023-06-16 17:22:34 +0900

    에러수정

--- src/main/frontapp/src/App.js
+++ src/main/frontapp/src/App.js
@@ -19,64 +19,64 @@ import { paresDate } from "./utils/parseDate";
 function App() {
     const [checkLogin, setCheckLogin] = useState(false);
 
-// 테스트용코드
-// const [checkLogin, setCheckLogin] = useState({
-//     userIdx: "2",
-//     userName : 'test',
-//     userGender : 'f',
-//     userEmail : "hakro1@gmail.com",
-//     userPhone : "01089456515"
-// });
+    // 테스트용코드
+    // const [checkLogin, setCheckLogin] = useState({
+    //     userIdx: "2",
+    //     userName : 'test',
+    //     userGender : 'f',
+    //     userEmail : "hakro1@gmail.com",
+    //     userPhone : "01089456515"
+    // });
 
-function App() {
-    const [checkLogin, setCheckLogin] = useState(false);
-    paresDate();
-    useEffect(() => {
-        // 페이지 로드 시 sessionStorage에서 checkLogin 값 가져오기
-        const storedCheckLogin = sessionStorage.getItem("checkLogin");
-        if (storedCheckLogin !== null) {
-            setCheckLogin(JSON.parse(storedCheckLogin));
-        }
-    }, []);
+    function App() {
+        const [checkLogin, setCheckLogin] = useState(false);
+        paresDate();
+        useEffect(() => {
+            // 페이지 로드 시 sessionStorage에서 checkLogin 값 가져오기
+            const storedCheckLogin = sessionStorage.getItem("checkLogin");
+            if (storedCheckLogin !== null) {
+                setCheckLogin(JSON.parse(storedCheckLogin));
+            }
+        }, []);
 
-    useEffect(() => {
-        // checkLogin 값이 변경될 때마다 sessionStorage에 저장
-        sessionStorage.setItem("checkLogin", JSON.stringify(checkLogin));
-    }, [checkLogin]);
+        useEffect(() => {
+            // checkLogin 값이 변경될 때마다 sessionStorage에 저장
+            sessionStorage.setItem("checkLogin", JSON.stringify(checkLogin));
+        }, [checkLogin]);
 
-    return (
-        <>
-            <Header checkLogin={checkLogin} setCheckLogin={setCheckLogin} />
+        return (
+            <>
+                <Header checkLogin={checkLogin} setCheckLogin={setCheckLogin} />
 
-            <main>
-                <Routes>
-                    <Route index element={<Main />} />
-                    <Route path="/category/*" element={<Category />} />
-                    <Route
-                        path="/login"
-                        element={<Login setCheckLogin={setCheckLogin} />}
-                    />
-                    <Route path="/signup" element={<Signup />} />
-                    <Route path="/findpw" element={<Findpw />} />
-                    <Route path="/cart" element={<Cart />} />
-                    <Route path="/purchase" element={<Purchase />} />
-                    <Route
-                        path="/mypage/*"
-                        element={
-                            <Mypage
-                                checkLogin={checkLogin}
-                                setCheckLogin={setCheckLogin}
-                            />
-                        }
-                    />
-                    <Route path="/detail" element={<Detail />} />
-                    <Route path="/admin/*" element={<Dashboard />} />
-                </Routes>
-            </main>
+                <main>
+                    <Routes>
+                        <Route index element={<Main />} />
+                        <Route path="/category/*" element={<Category />} />
+                        <Route
+                            path="/login"
+                            element={<Login setCheckLogin={setCheckLogin} />}
+                        />
+                        <Route path="/signup" element={<Signup />} />
+                        <Route path="/findpw" element={<Findpw />} />
+                        <Route path="/cart" element={<Cart />} />
+                        <Route path="/purchase" element={<Purchase />} />
+                        <Route
+                            path="/mypage/*"
+                            element={
+                                <Mypage
+                                    checkLogin={checkLogin}
+                                    setCheckLogin={setCheckLogin}
+                                />
+                            }
+                        />
+                        <Route path="/detail" element={<Detail />} />
+                        <Route path="/admin/*" element={<Dashboard />} />
+                    </Routes>
+                </main>
 
-            <Footer />
-        </>
-    );
+                <Footer />
+            </>
+        );
+    }
 }
-
-export default App;
+    export default App;
