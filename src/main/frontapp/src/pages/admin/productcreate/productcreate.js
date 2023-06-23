8cef99cec5cb689a6087331fa45f6c0d72840fe2
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2023-06-23 13:56:00 +0900

    이미지 서버 저장 및 DB저장2

--- src/main/frontapp/src/pages/admin/productcreate/productcreate.js
+++ src/main/frontapp/src/pages/admin/productcreate/productcreate.js
@@ -4,7 +4,6 @@ import "./productcreate.scss";
 import axios from "axios";
 import { paresDate } from "../../../utils/parseDate";
 
-
 function ProductCreate() {
     const navigate = useNavigate();
 
@@ -16,56 +15,67 @@ function ProductCreate() {
 
     useEffect(() => {
         axios
-        .get('/category')
-        .then((result) => {
-            setSize(result.data.csType);
-            setColor(result.data.ccType.map((ccType, index) =>({
-                ccType: ccType,
-                ccValMean: result.data.ccValMean[index],
-            })));
-            setTop(result.data.ctGrp.map((ctGrp, index)=> ({
-                ctGrp: ctGrp,
-                ctValMean: result.data.ctValMean[index],
-            })));
-            setMid(result.data.cmGrp.map((cmGrp, index)=>({
-                cmGrp: cmGrp,
-                cmValMean: result.data.cmValMean[index],
-            })));
-            setGender(result.data.genderCode.map((genderCode, index) => ({
-                genderCode: genderCode,
-                genderMean: result.data.genderMean[index],
-            })));
-        })
-        .catch((err)=> {
-            console.log(err);
-        })
-    }, [])
-    
-
-
+            .get("/category")
+            .then((result) => {
+                setSize(result.data.csType);
+                setColor(
+                    result.data.ccType.map((ccType, index) => ({
+                        ccType: ccType,
+                        ccValMean: result.data.ccValMean[index],
+                    }))
+                );
+                setTop(
+                    result.data.ctGrp.map((ctGrp, index) => ({
+                        ctGrp: ctGrp,
+                        ctValMean: result.data.ctValMean[index],
+                    }))
+                );
+                setMid(
+                    result.data.cmGrp.map((cmGrp, index) => ({
+                        cmGrp: cmGrp,
+                        cmValMean: result.data.cmValMean[index],
+                    }))
+                );
+                setGender(
+                    result.data.genderCode.map((genderCode, index) => ({
+                        genderCode: genderCode,
+                        genderMean: result.data.genderMean[index],
+                    }))
+                );
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        
-        const data = new FormData(e.currentTarget);
 
-        const productData = {
-            productName: data.get("productName"),
-            genderCode: data.get("genderCode"),
-            productPrice: data.get("productPrice"),
-            ccType: Array.from(data.getAll("ccType")).toString(),
-            csType: Array.from(data.getAll("csType")).toString(),
-            // productStock: data.get("productStock"),
-            ctGrp: data.get("ctGrp"),
-            cmGrp: data.get("cmGrp"),
-            productEnroll: paresDate(),
-        };
+        const formData = new FormData(e.currentTarget);
 
-        console.log(productData);
-        
+        const selectedCcTypes = Array.from(formData.getAll("ccType")).filter(
+            Boolean
+        );
+        formData.set("ccType", selectedCcTypes.join(","));
 
+        const selectedCsTypes = Array.from(formData.getAll("csType")).filter(
+            Boolean
+        );
+        formData.set("csType", selectedCsTypes.join(","));
+
+        formData.set("productEnroll", paresDate());
+
+        // 파일 첨부
+        const fileThumInput = e.currentTarget.elements.productThumUrl;
+        if (fileThumInput.files && fileThumInput.files.length > 0) {
+            formData.append("productThumUrl", fileThumInput.files[0]);
+        }
+        const fileDetailInput = e.currentTarget.elements.productDetail;
+        if (fileDetailInput.files && fileDetailInput.files.length > 0) {
+            formData.append("productDetail", fileDetailInput.files[0]);
+        }
         axios
-            .post("/product/create", productData)
+            .post("/product/create", formData)
             .then((response) => {
                 if (response.status === 200) {
                     alert("상품이 등록되었습니다.");
@@ -87,76 +97,90 @@ function ProductCreate() {
                     method="post"
                     className="productEditForm"
                     onSubmit={handleSubmit}
+                    encType="multipart/form-data"
                 >
-                    {/* <div className="productColumnTitle">
+                    <div className="productColumnTitle">
                         상품 썸네일 링크주소
                     </div>
-                    <input type="file" name="productThumUrl" /> */}
+                    <input type="file" name="productThumUrl" />
+
+                    <div className="productColumnTitle">상품상세이미지</div>
+                    <input type="file" name="productDetail" />
 
                     <div className="productColumnTitle">상품명</div>
                     <input type="text" name="productName" />
 
                     <div className="productColumnTitle">상품성별구분</div>
                     {gender.map((itemV) => {
-                            return(
-                                <label><input type ="radio" name="genderCode" value={itemV.genderCode}/>{itemV.genderMean}</label>
-                            );
-                        })}
+                        return (
+                            <label>
+                                <input
+                                    type="radio"
+                                    name="genderCode"
+                                    value={itemV.genderCode}
+                                />
+                                {itemV.genderMean}
+                            </label>
+                        );
+                    })}
                     <div className="productColumnTitle">상품가격</div>
                     <input type="text" name="productPrice" />
 
                     <div className="productColumnTitle">상품색상</div>
-                        {color.map((itemV) => {
-                            return(
-                                <label><input type ="checkbox" name="ccType" value={itemV.ccType}/>{itemV.ccValMean}</label>
-                            );
-                        })}
+                    {color.map((itemV) => {
+                        return (
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    name="ccType"
+                                    value={itemV.ccType}
+                                />
+                                {itemV.ccValMean}
+                            </label>
+                        );
+                    })}
 
                     <div className="productColumnTitle">상품사이즈</div>
                     {size.map((itemV) => {
-                            return(
-                                <label><input type ="checkbox" name="csType" value={itemV}/>{itemV}</label>
-                            );
-                        })}
-
-                    {/* <div className="productColumnTitle">상품재고량</div>
-                    <input type="number" name="productStock" /> */}
+                        return (
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    name="csType"
+                                    value={itemV}
+                                />
+                                {itemV}
+                            </label>
+                        );
+                    })}
 
                     <div className="productColumnTitle">상품대분류</div>
                     {top.map((itemV) => {
-                            return(
-                                <label><input type ="radio" name="ctGrp" value={itemV.ctGrp}/>{itemV.ctValMean}</label>
-                            );
-                        })}
-                    {/* <select name="ctGrp" defaultValue="n">
-                        <option value="n">NEW</option>
-                        <option value="s">SALE</option>
-                        <option value="b">BEST</option>
-                        <option value="m">MENS</option>
-                        <option value="l">LADIES</option>
-                    </select> */}
+                        return (
+                            <label>
+                                <input
+                                    type="radio"
+                                    name="ctGrp"
+                                    value={itemV.ctGrp}
+                                />
+                                {itemV.ctValMean}
+                            </label>
+                        );
+                    })}
 
                     <div className="productColumnTitle">상품중분류</div>
                     {mid.map((itemV) => {
-                            return(
-                                <label><input type ="radio" name="cmGrp" value={itemV.cmGrp}/>{itemV.cmValMean}</label>
-                            );
-                        })}
-                    {/* <select name="cmGrp" defaultValue="n">
-                        <option value="n">아우터</option>
-                        <option value="s">원피스</option>
-                        <option value="b">자켓</option>
-                        <option value="m">바지</option>
-                        <option value="l">스커트</option>
-                        <option value="l">양말</option>
-                    </select> */}
-
-                    {/* <div className="productColumnTitle">상품상세이미지</div>
-                    <input
-                        type="file"
-                        name="productDetail"
-                        multiple
-                    /> */}
+                        return (
+                            <label>
+                                <input
+                                    type="radio"
+                                    name="cmGrp"
+                                    value={itemV.cmGrp}
+                                />
+                                {itemV.cmValMean}
+                            </label>
+                        );
+                    })}
 
                     <button className="editProductBtn" type="submit">
                         제출
